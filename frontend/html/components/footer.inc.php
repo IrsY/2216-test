581cefd74c9675190956c3d892b889d681de7c5a
IrsY <muhdirsyad53@gmail.com>
2024-06-23 22:52:56 +0800

    update

--- frontend/html/components/footer.inc.php
+++
@@ -1,4 +0,0 @@
-<footer class="container text-center p-0">
-    <p><em>Copyright &copy; 2024 Keyboarder Pte. Ltd.</em></p>
-    <p><em>DISCLAIMER: THIS IS A EDUCATIONAL PROJECT WEBSITE</em></p>
-</footer>
--- frontend/html/process/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-return [
-    'servername' => 'mysql-active',  // or your server name
-    'username' => 'root',
-    'password' => 'jG31"8|y5{:Mj4o+Ga?~',
-    'dbname' => 'keyboarder'
-];
-?>
