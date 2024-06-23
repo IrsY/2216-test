edb27edbe2d874c6544f730aaea0bfd22d6c239b
IrsY <muhdirsyad53@gmail.com>
2024-06-23 22:49:22 +0800

    update

--- frontend/html/process/config.php
+++ frontend/html/process/config.php
@@ -4,6 +4,6 @@ return [
     'servername' => 'mysql-active',  // or your server name
     'username' => 'root',
     'password' => 'jG31"8|y5{:Mj4o+Ga?~',
-    'dbname' => 'mechkeys'
+    'dbname' => 'keyboarder'
 ];
 ?>
